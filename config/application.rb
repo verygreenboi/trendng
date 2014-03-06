2e2392f48add48a8ae382d0baa088d8d826249a5
Thompson Edolo <verygreenboi@live.com>
2014-03-06 17:31:22 +0100

    heroku fix

--- config/application.rb
+++ config/application.rb
@@ -5,15 +5,9 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
-
+config.assets.initialize_on_precompile = false
 module Trendng
   class Application < Rails::Application
-
-    config.generators do |g|
-      
-      
-    end
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
