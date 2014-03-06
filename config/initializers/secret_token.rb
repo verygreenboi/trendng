dbb175b25989e9bdfe62c19463de0f3294acdc2b
Thompson Edolo <verygreenboi@live.com>
2014-03-06 17:11:30 +0100

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trendng::Application.config.secret_key_base = 'd73c44815e4a256a42e419e52912dee4c3dd031be402dd9ac545f97f4b3840b1e8d73a6c097c04bad3bd27b36e253eb7816cbecd2d6ab2291827956ffdbc90c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Trendng::Application.config.session_store :cookie_store, key: '_trendng_session'
