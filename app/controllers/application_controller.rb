ee552016bf405b682cb9cea0acd10add74ebfb11
Thompson Edolo <verygreenboi@live.com>
2014-03-07 15:16:33 +0100

    Turbolink fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,20 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  layout "dashboard"
+  layout :layout_by_resource
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
+
+  protected
+  	def layout_by_resource
+
+  		if devise_controller?
+  			"dashboard"
+  		else
+  			"dashboard"
+  		end
+  	end
 
 end
