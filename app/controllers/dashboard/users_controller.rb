73655efc9c9ff7154c84f85e82fdfa7f7dbafe2a
Thompson Edolo <verygreenboi@live.com>
2014-03-08 17:41:22 +0100

    enable admin invitations

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -1,5 +1,6 @@
 class Dashboard::UsersController < ApplicationController
   before_filter :authenticate_user!
+  load_and_authorize_resource :only => [:index, :show, :new]
   def index
   end
 
@@ -16,8 +17,44 @@ class Dashboard::UsersController < ApplicationController
   end
 
   def update
+    authorize! :update, @user, :message => "Not authorized as an administrator"
+    user = User.find(params[:id])
+    if user.update_attributes(user_params, :as => :admin)
+      redirect_to dashboard_users_path, :notice => "User updated."
+    else
+      redirect_to dashboard_users_path, :alert => "User update failed."
+    end
   end
 
-  def delete
+  def destroy
+    authorize! :update, @user, :message => "Not authorized as an administrator"
+    user = User.find(params[:id])
+    unless user == current_user
+      user.destroy
+      redirect_to dashboard_users_path, :notice => "User removed."
+    else
+        redirect_to dashboard_users_path, :alert => "You can't delete yourself."
+    end
   end
+
+  def invite
+    authorize! :invite, @user, :message => "Not authorized as an administrator"
+    user = User.find(params[:id]).send_confirmation_instructions
+    redirect_to :back, :only_path => true, :notice => "Invitation Sent to #{@user.email}"
+  end
+
+  def bulk_invite
+    authorize! :bulk_invite, @user, :message => "Not authorized as an administrator"
+    users = User.where(:confirmation_token => nil).order(:created_at).limit(params[:quantity])
+    count = users.count
+    users.each do |user|
+      user.send_confirmation_instructions
+    end
+    redirect_to :back, :only_path => true, :notice => "Sent invitations to #{count} users."
+  end
+
+  private
+    def user_params
+      params.require(:user).permit!  
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 Trendng::Application.routes.draw do
 	namespace :dashboard do
 		match "", to: 'dashboard#index', via: :get 
-		resources :users
+		match "/users/bulk_invite/:quantity", to: 'users#bulk_invite', via: :get, :as => :bulk_invite
+		resources :users do
+			get 'invite', :on => :member
+		end
 	end
 	authenticated :user do
     root to: "dashboard/dashboard#index", as: :authenticated_root
   end
-  root :to => "home#index"
+  devise_scope :user do
+  	root :to => "home#index"
+  	match 'user/confirmation' to: 'confirmations#update', via: :put, :as => :update_user_confirmation
+	end
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users, only: [:show, :create]
 end
